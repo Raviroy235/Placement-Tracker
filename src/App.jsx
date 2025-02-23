43ac605b14ce685edf72d9c5538d9a5bfbd98cc9
Raviroy235 <naradasuraviteja235@gmail.com>
2025-02-23 10:34:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,28 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';  
 import Nav from './Components/Navbar/nav.jsx';
 import Home from './Components/Home/Home.jsx';
 import MyUserProfile from './Components/Myprofile/Myprofile.jsx';
-import BusinessEntity from './Components/BusinessEntity/BusinessEntity.jsx';
+import Opportunities from './Components/Opportunities/Opportunities.jsx';
 import UserReviews from './Components/UserReviews/UserReviews.jsx';
+import SignUp from './Components/SignUp/SignUp.jsx';
+import { UserProvider } from './Components/Context/UserContext';
+import Contact from './Components/Contact/Contact.jsx';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          
-          <Route
-            path="/"
-            element={
-              <div className="Home">
-                <Nav />
-                <Home />
-              </div>
-            }
-          />
-
-          <Route
-            path="/myprofile"
-            element={
-              <div className='prof'>
-                <Nav />
-                <MyUserProfile />
-              </div>
-            }
-          />
-
-          <Route
-          path='/company'
-          element={
-            <div className='company'>
-              <Nav />
-              <BusinessEntity />
-            </div>
-          }/>
-
-          <Route
-          path='/feedback'
-          element={
-            <div className='feedback'>
-              <Nav />
-              <UserReviews />
-            </div>
-          }/>
-
-          <Route
-            path="*"
-            element={
-              <div>
-                <p>kya bhai i am confused to route</p>
-              </div>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <UserProvider>
+      <Nav />  
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/myprofile" element={<MyUserProfile />} />
+        <Route path="/opportunities" element={<Opportunities />} />
+        <Route path="/feedback" element={<UserReviews />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<p>Page Not Found</p>} />
+      </Routes>
+    </UserProvider>
   );
 }
 
