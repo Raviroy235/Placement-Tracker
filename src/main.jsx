688087c8341f1b4e3bf19e977b73e40f67dfd632
Raviroy235 <naradasuraviteja235@gmail.com>
2025-02-23 10:37:15 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-   
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+  [
+    {
+      path: "/*",
+      element: <App />,
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true,  // Enables React 18's startTransition
+      v7_relativeSplatPath: true, // Updates relative route resolution for v7
+    },
+  }
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
