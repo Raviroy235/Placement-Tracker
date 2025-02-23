36748dd31d474b57b4dcefd7e9cd5d4054f0223d
Raviroy235 <naradasuraviteja235@gmail.com>
2025-02-23 10:05:32 +0530

    Update Company.jsx

--- src/Components/Company/Company.jsx
+++ src/Components/Company/Company.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import './Company.css';
-function Comp(){
-    const [count, setCount] = useState(0)
-    return(
-        <>
-        <div className="Comp">
-            Manage Your Company
-        </div>
 
+function ManageCompany() {
+    const [count, setCount] = useState(0);
+    return (
+        <>
+        <div className="companyManager">
+            Manage Your Business
+        </div>
         </>
-    )
+    );
 }
-export default Comp
+
+export default ManageCompany;
