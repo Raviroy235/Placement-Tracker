377daf7c2274a9d273c68d620fd6508f599c5bf0
Raviroy235 <naradasuraviteja235@gmail.com>
2025-02-23 10:32:37 +0530

    Create BussinessEntity.jsx

---
+++ src/Components/BussinessEntity.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BusinessEntity = () => {
+  return (
+    <div>
+      <h2>Business Entity Component</h2>
+      <p>This is the BusinessEntity component.</p>
+    </div>
+  );
+};
+
+export default BusinessEntity;
