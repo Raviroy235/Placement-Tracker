eeb0768a63bde931b4c642f6ad2594fec364959c
Raviroy235 <naradasuraviteja235@gmail.com>
2025-06-28 10:33:46 +0530

    Fixed typo in /opportunities route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Nav from './Components/Navbar/nav.jsx';
 import Home from './Components/Home/Home.jsx';
 import MyUserProfile from './Components/Myprofile/Myprofile.jsx';
-import Opportunities from './Components/Opportunities/Opportunities.jsx';
+import Opportunities from './Components/Opportunities.jsx';
 import UserReviews from './Components/UserReviews/UserReviews.jsx';
 import SignUp from './Components/SignUp/SignUp.jsx';
 import { UserProvider } from './Components/Context/UserContext';
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/myprofile" element={<MyUserProfile />} />
-        <Route path="/opportunities" element={<Opportunities />} />
+		<Route path="/opportunities" element={<Opportunities />} />
         <Route path="/feedback" element={<UserReviews />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<p>Page Not Found</p>} />
